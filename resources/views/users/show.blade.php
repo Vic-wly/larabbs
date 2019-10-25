7bc4d73e6afe90171279cb157fb4e5a2d01b6372
wanglaiyong <549608057@qq.com>
2019-10-25 07:12:29 +0000

    显示个人资料

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -10,11 +10,11 @@
     <div class="card ">
       <img class="card-img-top" src="http://b-ssl.duitang.com/uploads/item/201707/19/20170719211350_4PnBt.jpeg" alt="{{ $user->name }}">
       <div class="card-body">
-            <h5><strong>个人简介</strong></h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-            <hr>
-            <h5><strong>注册于</strong></h5>
-            <p>January 01 1901</p>
+        <h5><strong>个人简介</strong></h5>
+        <p>{{ $user->introduction }}</p>
+        <hr>
+        <h5><strong>注册于</strong></h5>
+        <p>{{ $user->created_at->diffForHumans() }}</p>
       </div>
     </div>
   </div>
