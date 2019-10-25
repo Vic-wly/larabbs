436edfb73b697e75cb2b5b7266badd40d7322a8d
wanglaiyong <549608057@qq.com>
2019-10-25 08:28:09 +0000

    显示头像

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,7 +8,7 @@
 
   <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
     <div class="card ">
-      <img class="card-img-top" src="http://b-ssl.duitang.com/uploads/item/201707/19/20170719211350_4PnBt.jpeg" alt="{{ $user->name }}">
+      <img class="card-img-top" src="{{ $user->avatar }}" alt="{{ $user->name }}">
       <div class="card-body">
         <h5><strong>个人简介</strong></h5>
         <p>{{ $user->introduction }}</p>
